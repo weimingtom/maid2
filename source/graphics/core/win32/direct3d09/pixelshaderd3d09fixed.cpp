e9bf6ea406b230f826476e3ede3070c0c8c751fa
renji2000 <renji2000@471aaf9e-aa7b-11dd-9b86-41075523de00>
2009-01-20 09:58:00 +0000

    SamplerStateの追加

--- source/graphics/core/win32/direct3d09/pixelshaderd3d09fixed.cpp
+++ source/graphics/core/win32/direct3d09/pixelshaderd3d09fixed.cpp
@@ -13,7 +13,7 @@ void PS_FIXED::Setup( const SPD3D09DEVICE& pDevice )
 {
   switch( m_No )
   {
-  case 0:
+  case 200:
     { //	頂点色をそのまま流す
 	    pDevice->SetTextureStageState( 0, D3DTSS_COLOROP,	D3DTOP_SELECTARG1 );
 	    pDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_DIFFUSE );
@@ -26,8 +26,10 @@ void PS_FIXED::Setup( const SPD3D09DEVICE& pDevice )
 	    pDevice->SetTextureStageState( 1, D3DTSS_ALPHAOP,	D3DTOP_DISABLE );
     }break;
 
-  case 10:
-    {//	テクスチャと頂点色の合成
+
+  case 210:
+    {
+      //	テクスチャと頂点色の合成
 	    pDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_MODULATE );
 	    pDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE );
 	    pDevice->SetTextureStageState( 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE );
@@ -39,19 +41,23 @@ void PS_FIXED::Setup( const SPD3D09DEVICE& pDevice )
 	    pDevice->SetTextureStageState( 1, D3DTSS_ALPHAOP,	D3DTOP_DISABLE );
     }break;
 
-  case 20:
-    {//	テクスチャと頂点色の合成, フォント用
-	    pDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_ADD );
+  case 211:
+    {
+      //	テクスチャと頂点色の合成
+      //  かつ、先にアルファをかけてしまう
+	    pDevice->SetTextureStageState( 0, D3DTSS_COLOROP, D3DTOP_MODULATE );
 	    pDevice->SetTextureStageState( 0, D3DTSS_COLORARG1, D3DTA_TEXTURE );
 	    pDevice->SetTextureStageState( 0, D3DTSS_COLORARG2, D3DTA_DIFFUSE );
-	    pDevice->SetTextureStageState( 1, D3DTSS_COLOROP,	D3DTOP_DISABLE );
 
-	    pDevice->SetTextureStageState( 0, D3DTSS_ALPHAOP, D3DTOP_MODULATE );
-	    pDevice->SetTextureStageState( 0, D3DTSS_ALPHAARG1, D3DTA_TEXTURE );
-	    pDevice->SetTextureStageState( 0, D3DTSS_ALPHAARG2, D3DTA_DIFFUSE );
+	    pDevice->SetTextureStageState( 1, D3DTSS_COLOROP,	D3DTOP_MODULATE );
+	    pDevice->SetTextureStageState( 1, D3DTSS_COLORARG1, D3DTA_CURRENT|D3DTA_ALPHAREPLICATE );
+	    pDevice->SetTextureStageState( 1, D3DTSS_COLORARG2, D3DTA_CURRENT );
+
+	    pDevice->SetTextureStageState( 2, D3DTSS_COLOROP,	D3DTOP_DISABLE );
+
+	    pDevice->SetTextureStageState( 0, D3DTSS_ALPHAOP,	D3DTOP_DISABLE );
 	    pDevice->SetTextureStageState( 1, D3DTSS_ALPHAOP,	D3DTOP_DISABLE );
     }break;
-
   }
 }
 
