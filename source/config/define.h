6d8431678f36286da9809c10e6de122543eac715
renji2000 <renji2000@gmail.com>
2010-03-25 03:51:35 +0000

    設定ファイルをユーザーが作れるように変更した

--- source/config/define.h
+++ source/config/define.h
@@ -5,6 +5,14 @@
 //	各種コンパイル設定を記述しておくヘッダ
 //	ライブラリの先頭で必ず include しておくこと
 
+
+
+//  使うユーザーが設定を変更できるように、設定ファイルを用意しました
+//  変更する場合は definelocal.h を各自で作成＆書き換えてください
+//  設定項目は↓の#defineが全部です
+#include"definelocal.h"
+
+/*
 #define GLOBAL_ASSERT   //!<	全部のソースでassertチェックを行う。
 #define GLOBAL_WARNING  //!<	全部のソースでwarning出力を行う。
 //#define GLOBAL_TRACE    //!<	全部のソースでトレースログの出力を使う。
@@ -15,19 +23,12 @@
 #define USE_DIRECT3DX9	//!<	d3dx9 の各種演算 を使用する
 
 
-//#define USE_INTELIPP		//!<	Intel Integrated Performance Primitives  を使用する
-
-
-
+#define USE_INTELIPP		//!<	Intel Integrated Performance Primitives  を使用する
+*/
 
 
 //////////	うっとおしい警告を表示しないようにする
 #pragma warning(disable:4996)	//	標準Ｃランタイムの関数名が古いと言われる
 #pragma warning(disable:4786)	//	map<vector,vector>とかでデバッグシンボルが長すぎる
-
-
-
-
-
 
 #endif
